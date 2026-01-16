82138a501f399baa4bf6b9500c1e0c49e612e358
Maciej Krzyżanowski <krzmaciek@gmail.com>
2026-01-16 23:02:30 +0100

    nvim: Setup go lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,3 +8,7 @@ vim.opt.wrap = false
 vim.opt.colorcolumn = "80"
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#444b6a" })
 vim.opt.scrolloff = 8
+vim.lsp.enable("gopls")
+vim.diagnostic.config({
+	virtual_text = true
+})
