0c277c1395d013070339f04214f4abbe833c8e52
Maciej Krzyżanowski <krzmaciek@gmail.com>
2026-01-17 12:43:43 +0100

    nvim: Improve autocompletions

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,3 +13,16 @@ vim.lsp.enable("gopls")
 vim.diagnostic.config({
 	virtual_text = true
 })
+
+-- Native way of setting up automcpletion
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client:supports_method('textDocument/completion') then
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--     end
+--   end,
+-- })
+-- 
+-- vim.cmd("set completeopt+=noselect")
+-- vim.opt.pumheight = 8
